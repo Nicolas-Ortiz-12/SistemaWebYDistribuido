4872307771204f6a89c4882a52a3f77dd68aa534
Nicolas <no684740@gmail.com>
2025-09-16 21:34:53 -0300

    Creacion del proyecto

---
+++ src/main/java/com/distri/proyectodistri/ProyectoDistriApplication.java
@@ -0,0 +1,13 @@
+package com.distri.proyectodistri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoDistriApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoDistriApplication.class, args);
+    }
+
+}
