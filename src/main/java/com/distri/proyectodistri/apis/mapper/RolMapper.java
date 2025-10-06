7cfb8077f8c35bdf087bc36d0a274924851686ee
CarlosM2404 <carlosmartinez@fiuni.edu.py>
2025-10-06 14:16:01 -0300

    Delete src directory

--- src/main/java/com/distri/proyectodistri/apis/mapper/RolMapper.java
+++
@@ -1,8 +0,0 @@
-package com.distri.proyectodistri.apis.mapper;
-
-import com.distri.proyectodistri.apis.entities.seguridad.Rol;
-import com.distri.proyectodistri.dto.RolDTO;
-import org.mapstruct.Mapper;
-
-@Mapper(config = MapperCentralConfig.class)
-public interface RolMapper extends BaseMapper<Rol, RolDTO> {}
--- src/main/java/com/distri/proyectodistri/apis/repository/ProveedorRepository.java
+++
@@ -1,7 +0,0 @@
-package com.distri.proyectodistri.apis.repository;
-
-import com.distri.proyectodistri.apis.entities.cliente.Proveedor;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProveedorRepository extends JpaRepository<Proveedor, Long> {
-}
